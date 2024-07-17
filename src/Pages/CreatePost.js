7764764e707b1c76fd1bc44d7ecb8679be83b733
Shruti Mishra <imshruti470@gmail.com>
2024-07-17 13:32:10 +0530

    form updated

--- src/Pages/CreatePost.js
+++ src/Pages/CreatePost.js
@@ -175,14 +175,14 @@ const CreatePost = () => {
                       htmlFor="price"
                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
-                      Price
+                      phone number
                     </label>
                     <input
                       type="text"
                       name="age"
                       id="price"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                      placeholder="age"
+                      placeholder="phone number"
                       required
                       onChange={(e) => setPrice(e.target.value)}
                     />
